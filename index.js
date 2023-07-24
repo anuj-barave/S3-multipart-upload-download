54f849502b655cac81be9e0a692e1ba03bbe9571
Anuj <anujbarave@gmail.com>
2023-07-24 18:48:10 +0530

    modified index.js

--- index.js
+++ index.js
@@ -131,82 +131,148 @@ const putObject = async (filePath, filename) => {
   };
 
   await client.send(new PutObjectCommand(params));
-  console.log("File uploaded successfully!");
+  printLog("File uploaded successfully!");
 };
 
 //fucntion to initiate multipartupload
 
-const initiateMultipartUpload = async (filePath, fileSize, filename) => {
+const timeout = (ms) =>
+  new Promise((_, reject) =>
+    setTimeout(() => reject(new Error("Timeout")), ms)
+  );
+
+const initiateMultipartUpload = async (
+  filePath,
+  fileSize,
+  filename,
+  total_parts
+) => {
+  const contentType = mime.lookup(filename);
+  const d = new Date();
+  let text = d.toISOString().substring(0, 16);
+  let keyforfilebe = text + "/" + filename;
   const params = {
     Bucket: BucketName,
-    Key: filename, // Replace with the destination path in your bucket
+    Key: keyforfilebe, // Replace with the destination path in your bucket
   };
 
-  const response = await client.send(new CreateMultipartUploadCommand(params));
-  const uploadId = response.UploadId;
-  console.log("Multipart upload initiated.");
-  const CHUNK_SIZE = 5 * 1024 * 1024;
+  try {
+    // Initiate the multipart upload
+    const response = await client.send(
+      new CreateMultipartUploadCommand(params)
+    );
+    const uploadId = response.UploadId;
+    printLog("Multipart upload initiated.");
+    const CHUNK_SIZE = 5 * 1024 * 1024;
 
-  const fileStream = fs.createReadStream(filePath, {
-    highWaterMark: CHUNK_SIZE,
-  });
-  let partNumber = 1;
-  const parts = [];
+    const fileStream = fs.createReadStream(filePath, {
+      highWaterMark: CHUNK_SIZE,
+    });
+    let partNumber = 1;
+    const parts = [];
 
-  // Function to upload a single part
-  const uploadPartParallel = async (uploadId, partNumber, body, filename) => {
-    const contentType = mime.lookup(filename);
-    const params = {
-      Bucket: BucketName,
-      Key: filename,
-      PartNumber: partNumber,
-      UploadId: uploadId,
-      Body: body,
-      ContentType: contentType,
-    };
-    try {
-      const response = await client.send(new UploadPartCommand(params));
-      console.log("Uploaded ", partNumber, " part on S3");
-      return response.ETag;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // Array to store all the upload promises
-  const uploadPromises = [];
-
-  for await (const chunk of fileStream) {
-    // Start uploading the part in parallel
-    const uploadPromise = uploadPartParallel(
+    // Function to upload a single part
+    const uploadPartParallel = async (
       uploadId,
       partNumber,
-      chunk,
-      filename
-    );
-    uploadPromises.push(uploadPromise);
-    partNumber++;
+      body,
+      keyforfilebe,
+      contentType,
+      total_parts
+    ) => {
+      const params = {
+        Bucket: BucketName,
+        Key: keyforfilebe,
+        PartNumber: partNumber,
+        UploadId: uploadId,
+        Body: body,
+        ContentType: contentType,
+      };
+
+      try {
+        printLog(
+          "Upload started for part " + partNumber + " of " + total_parts
+        );
+        const response = await client.send(new UploadPartCommand(params));
+        printLog(
+          "Upload was successful for part " + partNumber + " of " + total_parts
+        );
+        return response.ETag;
+      } catch (err) {
+        printLog(err);
+      }
+    };
+
+    // Array to store all the upload promises
+    const uploadPromises = [];
+
+    printLog("Initiated process of dividing the files into chunks of 5 MB");
+    for await (const chunk of fileStream) {
+      // Start uploading the part in parallel, with a timeout of 30 seconds for each part upload
+      const uploadPromise = Promise.race([
+        uploadPartParallel(
+          uploadId,
+          partNumber,
+          chunk,
+          keyforfilebe,
+          contentType,
+          total_parts
+        ),
+        timeout(180000), // 180 seconds timeout for each part upload (adjust as needed)
+      ]);
+
+      printLog("Adding promises for part " + partNumber + " of " + total_parts);
+      if (partNumber % 5 == 0) {
+        var dividebyzeroerror = 1 / 0;
+      }
+      uploadPromises.push(uploadPromise);
+      partNumber++;
+    }
+
+    // Wait for all the upload promises to be resolved
+    const uploadResults = await Promise.all(uploadPromises);
+
+    // Process the upload results and add parts to the parts array in order
+    uploadResults.forEach((part, index) => {
+      parts.push({ PartNumber: index + 1, ETag: part });
+    });
+
+    printLog("All parts have been uploaded");
+    printLog("Calling complete event now on AWS S3 for this file");
+    // calling Complete the multipart upload function
+    await completeMultipartUpload(uploadId, parts, keyforfilebe, contentType);
+  } catch (error) {
+    // Handle any errors that occur during the upload process
+    printLog("Error during the upload:" + error.message);
   }
-
-  // Wait for all the upload promises to be resolved
-  const uploadResults = await Promise.all(uploadPromises);
-
-  // Process the upload results and add parts to the parts array in order
-  uploadResults.forEach((part, index) => {
-    parts.push({ PartNumber: index + 1, ETag: part });
-  });
-
-  // Complete the multipart upload
-  await completeMultipartUpload(uploadId, parts, filename);
 };
 
 //function for completion of multipart upload
+var logarr = [];
 
-const completeMultipartUpload = async (uploadId, parts, filename) => {
-  const contentType = mime.lookup(filename);
+const printLog = (logtext) => {
+  var d = new Date();
+  datetext = d.toTimeString();
+  datetext = datetext.split(" ")[0];
+  var logtext = datetext + " : " + logtext;
+  console.log(logtext);
+  logarr.push(logtext);
+};
+
+const storelog = () => {
+  //send storelogs to aws lambda. awslambda will store the logs into postgresql.
+};
+
+const completeMultipartUpload = async (
+  uploadId,
+  parts,
+  keyforfilebe,
+  contentType
+) => {
+  printLog("Part upload complete event is being fired");
   const params = {
     Bucket: BucketName,
-    Key: filename,
+    Key: keyforfilebe,
     MultipartUpload: {
       Parts: parts,
     },
@@ -214,43 +280,70 @@ const completeMultipartUpload = async (uploadId, parts, filename) => {
     ContentType: contentType,
   };
 
-  await client.send(new CompleteMultipartUploadCommand(params));
-  console.log("Multipart upload completed successfully!");
+  const response = await client.send(
+    new CompleteMultipartUploadCommand(params)
+  );
+
+  printLog(
+    "Multipart upload completed successfully & uploaded file Successfully"
+  );
 };
 
 //post api request to upload a file on S3
 
 app.post("/upload", upload.single("file"), async (req, res) => {
-  console.log("upload file request arrived");
+  printLog("upload file request arrived");
   const uploadedFile = req.file;
   const filepath = uploadedFile.path;
   const filename = uploadedFile.originalname;
-
   const fileSize = getFileSize(uploadedFile.path);
   try {
     if (fileSize) {
       if (fileSize <= PART_SIZE) {
         // Directly upload the file to S3
-        console.log("Directly uploading the file to S3");
+        printLog("filesize is less than 5 MB");
+        printLog("Directly uploading the file to S3");
         await putObject(filepath, filename);
         fs.unlink(uploadedFile.path, (err) => {
           if (err) {
-            console.error("Error deleting file:", err);
+            printLog("Error deleting file:" + err);
           } else {
-            console.log("File deleted from server:", uploadedFile.path);
+            printLog("File deleted from server:" + uploadedFile.path);
           }
         });
       } else {
         // Initiate multipart upload
-        await initiateMultipartUpload(filepath, fileSize, filename);
+        printLog("filesize is greater than 5 MB");
+        let fileSizeinMB = Math.ceil(fileSize / (1024 * 1024));
+        let total_parts = Math.ceil(fileSizeinMB / 5);
+        let print =
+          "We Divided total file size of " +
+          fileSizeinMB +
+          " MB in " +
+          total_parts +
+          " parts";
+        printLog(print);
+        await initiateMultipartUpload(
+          filepath,
+          fileSize,
+          filename,
+          total_parts
+        );
         fs.unlink(uploadedFile.path, (err) => {
           if (err) {
-            console.error("Error deleting file:", err);
+            printLog("Error deleting file:" + err);
           } else {
-            console.log("File deleted from server:", uploadedFile.path);
+            printLog("File deleted from server:" + uploadedFile.path);
           }
         });
       }
+      // printLog(
+      //   "------------------------------------------------------------------------"
+      // );
+      // printLog("Printing the Log Array file");
+      // logarr.forEach(function (entry) {
+      //   console.log(entry);
+      // });
       res.send("file uploaded Succesfully");
     }
   } catch (err) {
@@ -258,5 +351,14 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   }
 });
 
+// process.on("uncaughtException", (error) => {
+//   // Log the uncaught exception before the application goes down
+//   console.error("Uncaught Exception:", error.stack);
+
+//   // Optionally, you can write the error to a log file using a logging library or native fs module
+
+//   // Gracefully exit the application after logging the error
+//   process.exit(1);
+// });
 PORT = 3100;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
